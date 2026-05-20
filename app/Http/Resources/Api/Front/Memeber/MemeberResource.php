9a4215f32ce39fff602bb99b00677ca098cee6b0
Mohamed Hamed <muhmdhamed757@gmail.com>
2026-05-21 02:02:04 +0300

    profile data

--- app/Http/Resources/Api/Front/Memeber/MemeberResource.php
+++ app/Http/Resources/Api/Front/Memeber/MemeberResource.php
@@ -23,7 +23,7 @@ class MemeberResource extends JsonResource
             'points'=>$this->points,
             'profile_completed'=> (bool) ($this->profile?->government && $this->profile?->address),
             'profile'=> [
-                'government' => ['name_ar' => $this->profile?->government?->name_ar , 'en_name' => $this->profile?->government?->en_name],
+                'government' => ['name_ar' => $this->profile?->government?->name_ar , 'name_en' => $this->profile?->government?->name_en],
                 'address' => $this->profile?->address,
                 'city' => $this->profile?->city,
                 'area' => $this->profile?->area,
