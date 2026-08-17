dd0e149e17e6b5b326221fbf046f1713573bc5cd
Mohamed Hamed <cangrowonline1@gmail.com>
2026-08-17 16:02:26 +0300

    fix foregin key in mutli id

--- routes/api/admin/promotion.php
+++ routes/api/admin/promotion.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::namespace('Ecommerce\Promotion')->group(function () {
- Route::post('store-promotion' , 'PromotionController@storePromotion')->middleware('checkEcommercePermision:promotion,create:create');
- Route::post('update-promotion' , 'PromotionController@updatePromotion')->middleware('checkEcommercePermision:promotion,update:update');
- Route::post('delete-promotion' , 'PromotionController@deletePromotion')->middleware('checkEcommercePermision:promotion,delete:delete');
- Route::post('promotion-details' , 'PromotionController@promotionDetails')->middleware('checkEcommercePermision:promotion,view:view');
- Route::post('get-promotions' , 'PromotionController@getPromotions')->middleware('checkEcommercePermision:promotion,view:view');  
+ Route::post('store-promotion' , 'PromotionController@storePromotion')->middleware('checkEcommercePermision:promotion,create');
+ Route::post('update-promotion' , 'PromotionController@updatePromotion')->middleware('checkEcommercePermision:promotion,update');
+ Route::post('delete-promotion' , 'PromotionController@deletePromotion')->middleware('checkEcommercePermision:promotion,delete');
+ Route::post('promotion-details' , 'PromotionController@promotionDetails')->middleware('checkEcommercePermision:promotion,view');
+ Route::post('get-promotions' , 'PromotionController@getPromotions')->middleware('checkEcommercePermision:promotion,view');  
 
 
 
